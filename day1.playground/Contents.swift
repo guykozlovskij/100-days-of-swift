e40fc66771e2ac913e903d16ef0a15a73abb3e1c
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2025-05-06 10:17:52 +0100

    initial

---
+++ day1.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+print("Hello \(name) ")
+
+let character = "Daphne"
+//...
+character.uppercased()
+print(character)
+
+var int = 1000
+int = int + 5000
+print(int)
+
+
+let bigNumber = 100_000_000_000
+   
---
+++ day2.playground/Contents.swift
@@ -0,0 +1,20 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+let truth = true
+
+let msg = "test"
+
+print("hello world \(msg)")
+//math can be done in string interpolation)
+
+
+
+let temperatureCelsius = 10
+      
+let temperatureFarenheit = ((temperatureCelsius * 9) / 5) + 32
+      
+print("Celsius is \(temperatureCelsius)")
+print("Farenheit is \(temperatureFarenheit)")
+      
