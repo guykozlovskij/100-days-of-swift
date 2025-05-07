a1b76782e12e5843c801da7259ce6b869461ba71
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2025-05-07 07:37:34 +0100

    finish day 4

---
+++ day4.playground/Contents.swift
@@ -0,0 +1,34 @@
+import Cocoa
+
+// Type annotations
+
+let name: String = "Guy"
+let myNum: Int = 123
+
+var strArray: [String] = ["a", "b"]
+
+var annontatedDict: [String: Int] = ["id": 123]
+
+var annotatedSet: Set<String> = Set(["a", "b", "c"])
+
+// initialising an empty annotated array
+
+var teams = [String]()
+
+// also the following work
+// var teams: [String] = [String]()
+// var teams: [String] = []
+
+teams = ["some team"]
+print(teams)
+
+enum UIStyle {
+    case light, dark, system
+}
+
+var style: UIStyle = UIStyle.light
+style = .dark
+
+let username: String
+username = "gkozl"
+  
