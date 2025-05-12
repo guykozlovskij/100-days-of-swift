5c51eb887de2b8cbc0c8921accb7b8f31ebe613e
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2025-05-12 19:00:16 +0100

    finish day 5

--- day5.playground/Contents.swift
+++ day5.playground/Contents.swift
@@ -6,6 +6,7 @@ if score > 80 {
     print("Great Job!")
 }
 
+
 let age = 17
 
 if age >= 18 {
@@ -14,11 +15,13 @@ if age >= 18 {
     print("not of age")
 }
 
+
 let temp = 25
 
 if temp > 20 && temp < 30 {
     print("nice weather")
 }
+
 
 let userAge = 14
 let hasParentalConsent = true
@@ -27,16 +30,39 @@ if age >= 18 || hasParentalConsent {
     print("can buy game")
 }
 
+
 enum TransportOption {
     case airplane, helicopter, bicycle, car, escooter
 }
 
 let transport = TransportOption.escooter
 
-if transport == .airplane || transport == .helicopter {
+//if transport == .airplane || transport == .helicopter {
+//    print("Let's fly")
+//} else if transport == .bicycle {
+//    print("let's cy cle")
+//} else {
+//    print("scooter or car")
+//}
+ 
+let transportToday = TransportOption.escooter
+
+switch transportToday {
+case .airplane:
     print("Let's fly")
-} else if transport == .bicycle {
+case .bicycle:
     print("let's cycle")
-} else {
-    print("scooter or car")
+case .car:
+    print("car")
+default:
+    print("others")
+//case .helicopter:
+//    print("Let's fly")
+//case .escooter:
+//    print("Let's scoot")
 }
+
+let newAge = 18
+
+let canVote = age >= 18 ? "can vote" : "cannot vote"
+print(canVote)
