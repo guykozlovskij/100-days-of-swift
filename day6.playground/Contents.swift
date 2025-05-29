1cef1afc8056694e245babaf33b56c351eecca0f
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2025-05-29 10:28:47 +0100

    finish day 6

--- day6.playground/Contents.swift
+++ day6.playground/Contents.swift
@@ -8,7 +8,7 @@ for os in platforms {
 
 // New data type -> range 1...12
 for i in 1...12 {
-    print ("5 x \(i) is \(5 * i)")
+    print("5 x \(i) is \(5 * i)")
 }
 
 for i in 1..<5 {
@@ -17,14 +17,12 @@ for i in 1..<5 {
 
 var lyric = "Haters gonna"
 
-
 // Underscore can be used when you don't actually need a value as you're going through a loop
 for _ in 1...5 {
     lyric += " hate"
 }
 
 print(lyric)
-
 
 var countdown = 10
 
@@ -46,3 +44,32 @@ while roll != 20 {
 }
 
 print("Critical hit")
+
+let filenames = ["me.jpg", "work.txt", "sohpie.jpg"]
+
+for filename in filenames {
+    if filename.hasSuffix(".jpg") == false {
+        continue
+    }
+    print("found picture: \(filename)")
+}
+
+let number1 = 4
+let number2 = 14
+
+var multiples = [Int]()
+
+for i in 1...100_000 {
+    if i.isMultiple(of: number1) && i.isMultiple(of: number2) {
+        multiples.append(i)
+
+        if multiples.count == 10 {
+            break
+        }
+    }
+}
+
+print(multiples)
+
+// use continue when we want to skip an iteration that does not meet condition.
+// use break when you want to end once some condition is meat
