cc9bd9300a623236251cc780837bcd8f41718a53
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2025-06-02 16:49:08 +0100

    day 8

---
+++ day8.playground/Contents.swift
@@ -0,0 +1,41 @@
+import Cocoa
+
+// providing a default value to a function
+func printTimesTables(for number: Int, end: Int = 12) {
+    for i in 1...end {
+        print(i * number)
+    }
+}
+
+// only first param is needed
+printTimesTables(for: 5)
+
+// Error Handling
+enum PasswordError: Error {
+    case short, obivious
+}
+
+func checkPassword(password: String) throws -> String {
+    if password.count < 5 { throw PasswordError.short }
+    if password == "12345" { throw PasswordError.obivious }
+
+    if password.count < 8 {
+        return "OK"
+    } else if password.count < 10 {
+        return "Good"
+    } else {
+        return "Excellent"
+    }
+}
+
+
+let string = "scfdslkgfdlkbfdvldfs;"
+
+do {
+    let result = try checkPassword(password: string)
+    print("Password rating: \(result)")
+} catch {
+    print("There was an error")
+}
+
+
